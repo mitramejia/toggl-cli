d991c6c2ef6aa228d84fc4a27aed29d29588ea02
Mitra Mejia <mmejia@wunderkind.nyc>
2020-07-27 18:38:10 -0400

    Rename api/auth to api/users

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import {isUndefined} from 'lodash'
 import {TOGGL_API_URL, TOGGL_API_TOKEN_NOT_SET_ERROR} from './constants'
 import {CLIError} from '@oclif/errors'
+import {User} from '../..'
 
 export const paths = {
   sessions: '/sessions',
