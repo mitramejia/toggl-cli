aa8ae42bf8fdac19a0d2454c9a57afa2179b5a57
Mitra Mejia <mmejia@wunderkind.nyc>
2020-09-02 21:19:57 -0400

    Show project name in stop command log

--- src/commands/entry/stop.ts
+++ src/commands/entry/stop.ts
@@ -1,13 +1,17 @@
 import {Command, flags} from '@oclif/command'
 import {getCurrentTimeEntry, stopTimeEntry} from '../../api/time-entry'
 import {Commands} from '../../api/constants'
+import {bold} from 'chalk'
+import {getProjectById, getUserProjects} from '../../utils/project'
+import {formatProjectName} from '../../utils/time-entry'
+import {Project} from '../../..'
 
 export default class EntryStop extends Command {
     static description = 'Stops current time entry'
 
     static strings = {
       noTimeEntryRunning: 'You have no time entry running.',
-      entryStopped: (entryDesc: string) => `Stopped time entry '${entryDesc}'`,
+      entryStopped: (entryDesc: string, project?: Project) => `Stopped time entry '${bold(entryDesc)}'${formatProjectName(project)}`,
     }
 
     static flags = {
@@ -19,15 +23,18 @@ export default class EntryStop extends Command {
     async run() {
       try {
         // Get current time entry
-        const currentTimeEntry = await getCurrentTimeEntry()
+        const timeEntry = await getCurrentTimeEntry()
         // Exit if there is no current time entry
-        if (!currentTimeEntry) {
+        if (!timeEntry) {
           this.error(EntryStop.strings.noTimeEntryRunning)
         }
-        // Call API to Start time entry
-        await stopTimeEntry(currentTimeEntry.id)
+        const projects = await getUserProjects()
+        const project = timeEntry.pid ? getProjectById(timeEntry.pid, projects) : undefined
 
-        this.log(EntryStop.strings.entryStopped(currentTimeEntry.description))
+        // Call API to stop the time entry
+        await stopTimeEntry(timeEntry.id)
+
+        this.log(EntryStop.strings.entryStopped(timeEntry.description, project))
 
         await this.config.runHook(Commands.EntryStop, {id: this.id})
       } catch (error) {
